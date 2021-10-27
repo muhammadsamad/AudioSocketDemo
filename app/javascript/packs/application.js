794e805bd1a9187508102bf4761370c9bb66be66
muhammadsamad <muhammad.samad@square63.org>
2021-10-27 11:13:43 +0500

    Created sign-up and sign-in form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,22 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+// import "bootstrap"
+// import "../stylesheets/application"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
+
+
+import 'bootstrap'
+import "chartkick/chart.js"
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")

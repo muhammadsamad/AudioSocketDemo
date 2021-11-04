2aa0a821c5997be346c756cf61876f5c625c3ffe
muhammadsamad <muhammad.samad@square63.org>
2021-11-04 18:09:41 +0500

    Made ROLES constant

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   ARTIST = 'Artist'.freeze
   MANAGER = 'Manager'.freeze
-  enum role: [ARTIST, MANAGER]
+  ROLES = [ARTIST, MANAGER].freeze
+  enum role: ROLES
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
